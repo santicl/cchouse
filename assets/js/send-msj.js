55444fc0a63247ee7233381787d52f303917c553
santiago <caballerosuarezsantiago@gmail.com>
2022-08-09 17:59:16 -0500

    U files U

--- assets/js/send-msj.js
+++ assets/js/send-msj.js
@@ -3,8 +3,9 @@ const sendMsj = (e) => {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let rooms = document.getElementById("rooms").value;
+    let childrens = document.getElementById("children").value;
 
-    const URL_API = 'https://api.whatsapp.com/send?phone=573215398340&text=Hola%20%F0%9F%98%80%2C%20mi%20nombre%20es%20' + name + '%2C%20estoy%20interesado(a)%20en%20' + rooms + '%2C%20si%20desea%20enviarme%20mas%20informaci%C3%B3n%2C%20lo%20puede%20enviar%20al%20correo%20' + email + '.';
+    const URL_API = 'https://api.whatsapp.com/send?phone=573215398340&text=Hola%20%F0%9F%98%80%2C%20mi%20nombre%20es%20' + name + '%2C%20estoy%20interesado(a)%20en%20' + rooms + '%2C%20%20Nuestra reserva%2C%20' + childrens + '%2C%20si%20desea%20enviarme%20mas%20informaci%C3%B3n%2C%20lo%20puede%20enviar%20al%20correo%20' + email + '.';
     document.getElementById("form").reset();
     window.open(URL_API, '_blank');
 }
