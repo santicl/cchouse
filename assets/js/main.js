546849d92ef15851966059abed7dae82d39b513d
santiago <caballerosuarezsantiago@gmail.com>
2022-08-16 00:52:30 -0500

    new file: ubications

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,9 +182,20 @@
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
+      // filter items when page load
+
+      portfolioFilters.forEach(function(el) {
+        portfolioIsotope.arrange({
+          filter: el.getAttribute('data-filter')
+        });
+      });
+
+      // filter items on "button" click
+
       on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
+          console.log(el)
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
